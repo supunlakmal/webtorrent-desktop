632d2ea91a575d26c2e3817b7ae00fe2506a3e7c
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2022-08-27 11:33:06 +0530

    update

--- src/renderer/webtorrent.js
+++ src/renderer/webtorrent.js
@@ -103,8 +103,8 @@ function setGlobalTrackers (globalTrackers) {
 // Starts a given TorrentID, which can be an infohash, magnet URI, etc.
 // Returns a WebTorrent object. See https://git.io/vik9M
 function startTorrenting (torrentKey, torrentID, path, fileModtimes, selections) {
-  // console.log('+++++',torrentKey, torrentID, path, fileModtimes, selections)
-  // console.log('starting torrent %s: %s', torrentKey, torrentID)
+  console.log('+++++',torrentKey, torrentID, path, fileModtimes, selections)
+  console.log('starting torrent %s: %s', torrentKey, torrentID)
 
   // const torrent = client.add(torrentID, {
   //   path,
